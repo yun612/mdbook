2fe351b6db5c18a73183a4d69859bd01369111b8
hl <hanlei@fourz.cn>
2025-01-09 18:33:05 +0800

    update:adjust editor component

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -4,7 +4,7 @@ import MonacoEditor from 'monaco-editor-vue3'
 import { useClipboard } from '@vueuse/core'
 import { useAppStore } from '@/stores/app'
 
-const { copy,copied } = useClipboard({})
+const { copy, copied } = useClipboard({})
 const store = useAppStore()
 const currentOutput = computed(() => store.outputs[store.editor.lang])
 
@@ -18,15 +18,17 @@ watch(() => store.editor.code, (newCode) => {
 
 const editorOptions = {
   fontSize: 16,
-  lineNumbers: 'off',
+  lineNumbers: 'on',
   minimap: { enabled: false },
-  scrollBeyondLastLine: false,
+  scrollBeyondLastLine: true,
   automaticLayout: true,
   tabSize: store.editor.lang === 'python' ? 4 : 2,
-  wordWrap: 'on'
+  wordWrap: 'on',
+  lineNumbersMinChars: 2,
+  renderLineHighlight: 'all',
+  lineDecorationsWidth: 0,
 }
 const handleChange = (value: string) => {
-  console.log('code changed', code.value)
   store.updateEditor({ code: value })
 }
 
@@ -51,9 +53,9 @@ onUnmounted(() => {
 })
 
 function clearOutput() {
+  store.clearOutput()
   store.editor.code = initCode
   code.value = initCode
-  store.clearOutput()
 }
 </script>
 
@@ -63,10 +65,14 @@ function clearOutput() {
     <div class="flex items-center justify-between">
       <p class="text-lg font-semibold text-white  rounded-md">Python Editor</p>
       <div class="flex items-center gap-x-4">
-        <span v-if="copied" class="text-green-600">Copied successfully!</span>
+        <span v-if="copied" class="text-green-300 text-xs">Copied !</span>
         <button class="border p-1 rounded-md border-gray-500 text-gray-400 hover:text-gray-200"
           @click="copy(store.editor.code)">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24">
+          <svg v-if="copied" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24">
+            <path fill="currentColor"
+              d="m9.55 15.15l8.475-8.475q.3-.3.7-.3t.7.3t.3.713t-.3.712l-9.175 9.2q-.3.3-.7.3t-.7-.3L4.55 13q-.3-.3-.288-.712t.313-.713t.713-.3t.712.3z" />
+          </svg>
+          <svg v-else xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24">
             <path fill="currentColor"
               d="M9 18q-.825 0-1.412-.587T7 16V4q0-.825.588-1.412T9 2h9q.825 0 1.413.588T20 4v12q0 .825-.587 1.413T18 18zm0-2h9V4H9zm-4 6q-.825 0-1.412-.587T3 20V6h2v14h11v2zm4-6V4z" />
           </svg>
@@ -81,8 +87,7 @@ function clearOutput() {
           </svg>
         </button>
 
-        <button 
-          class="border p-1 rounded-md border-gray-500 text-gray-400 hover:text-gray-200" >
+        <button class="border p-1 rounded-md border-gray-500 text-gray-400 hover:text-gray-200">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" @click="store.executeCode">
             <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
               d="M7 4v16l13-8z" />
@@ -104,8 +109,8 @@ function clearOutput() {
         </button>
       </div>
     </div>
-    <p class="border-t my-2"></p>
-    <MonacoEditor class="min-h-32" language="python" v-model:value="code" :options="editorOptions"
+    <p class="border-t my-2 border-gray-400"></p>
+    <MonacoEditor class="min-h-40" language="python" v-model:value="code" :options="editorOptions"
       :theme="store.editor.theme === 'dark' ? 'vs-dark' : 'vs'" @change="handleChange" />
 
     <div class="output-wrapper dark:bg-zinc-900 py-3">
@@ -125,14 +130,13 @@ function clearOutput() {
 
 
 <style>
-
 .output-list {
   display: flex;
   flex-direction: column;
 }
 
 .output-item {
-  padding: 0.3rem;
+  padding-inline: 0.2rem;
   border-radius: 0.375rem;
   font-family: monospace;
 }
